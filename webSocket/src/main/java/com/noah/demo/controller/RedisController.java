17a15c0a85b597f6ce951caaf5274538142f6a1f
noah <lifeidll@163.com>
2022-07-21 16:48:02 +0800

    redis

---
+++ webSocket/src/main/java/com/noah/demo/controller/RedisController.java
@@ -0,0 +1,13 @@
+package com.noah.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @ClassName RedisController
+ * @Description TODO
+ * @Author noah
+ * @Date 2022/7/13 10:00
+ * @Version 1.0
+ **/
+public class RedisController {
+}
